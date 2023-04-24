92a908bc149e225871192e158a1cb5ea851acb06
Jay Long <jaylong255@gmail.com>
2023-04-24 12:43:28 -0500

    update readme, instantiate openai

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # prompt-engineering
 Experimenting with prompt engineering. 
+
+codename `lsb`, which is short for lightsaber since i see the ability for humans to effectively harness 
+narrow ai in an advanced way by coding as akin to a jedi building a lightsaber. while others can use
+a jedi's saber, it was clearly built by and for a specific force-sensitive person. plus it sounds trippy.
---
+++ lsb.py
@@ -0,0 +1,18 @@
+import openai
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import os
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set up the OpenAI API
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+# Set up the LLMS
+llms = OpenAI(model_name="text-davinci-003", temperature=0)
+
+
